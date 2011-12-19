b9218d266d5492b68253ce2d186a0c34ce10fa6b
Stephen Wyatt Bush <stephen.wyatt@gmail.com>
2011-12-19 16:18:43 -0500

    exercise 1

---
+++ ex1.sql
@@ -0,0 +1,18 @@
+CREATE TABLE person (
+  id INTEGER PRIMARY KEY,
+  first_name TEXT,
+  last_name TEXT,
+  age INTEGER
+);
+
+-- extra credit
+-- case sensitivity
+
+create table person_downcase (
+  id integer primary key,
+  first_name text,
+  middle_name text,
+  last_name text,
+  weight float,
+  age integer
+);
